a44d24dcd96f642f5321a30c7dba53067f97c10c
Sergio Guachalla <sergioguachalla0@gmail.com>
2023-10-12 15:16:41 -0400

    adding ng prime and keycloak service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,55 @@
-import { NgModule } from '@angular/core';
+import { NgModule, APP_INITIALIZER} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ButtonModule } from 'primeng/button';
+import { KeycloakAngularModule, KeycloakService } from 'keycloak-angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import {TabMenuModule} from "primeng/tabmenu";
+import { HomeComponent } from './components/shared/home/home.component';
+import {MenubarModule} from "primeng/menubar";
+
+function initializeKeycloak(keycloak: KeycloakService) {
+  return () =>
+    keycloak.init({
+      config: {
+        url: 'http://localhost:8080',
+        realm: 'cognito-hub',
+        clientId: 'frontend'
+      },
+      initOptions: {
+        onLoad: 'check-sso',
+        checkLoginIframe: false,
+        silentCheckSsoRedirectUri:
+          window.location.origin + '/assets/silent-check-sso.html'
+      }
+    });
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule,
+    TabMenuModule,
+    MenubarModule,
+    KeycloakAngularModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeKeycloak,
+      multi: true,
+      deps: [KeycloakService]
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -0,0 +1,35 @@
+import {Component, inject, Inject, OnInit} from '@angular/core';
+import {MenuItem} from "primeng/api";
+import {HomeComponent} from "../home/home.component";
+import {KeycloakService} from "keycloak-angular";
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit{
+
+  items : MenuItem[] | undefined;
+  activeItem: MenuItem | undefined;
+
+  keycloakService: KeycloakService = inject(KeycloakService);
+
+  ngOnInit() {
+    this.items = [
+      { label: 'Home', icon: 'pi pi-fw pi-home', routerLink: ['/home'] },
+      { label: 'Cursos', icon: 'pi pi-fw pi-calendar'},
+      { label: 'Iniciar sesión', icon: 'pi pi-fw pi-user',  command: () => this.login()  },
+      { label: 'Registrarse', icon: 'pi pi-fw pi-user-plus',  routerLink: '/home'}
+
+    ];
+    this.activeItem = this.items[0];
+  }
+
+
+  login() {
+    this.keycloakService.login();
+    console.log("login");
+  }
+
+}
