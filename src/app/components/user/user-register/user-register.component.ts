33d470c54d2a27cfd6e8d9bc0da09d4d66bb206c
Sergio Guachalla <sergioguachalla0@gmail.com>
2023-10-22 23:40:21 -0400

    user form password validation

--- src/app/components/user/user-register/user-register.component.ts
+++ src/app/components/user/user-register/user-register.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup} from "@angular/forms";
+import {SubCategoryDto} from "../../../model/dto/SubCategoryDto";
+import {LanguageDto} from "../../../model/dto/LanguageDto";
+import {LevelDto} from "../../../model/dto/LevelDto";
+import {Message} from "primeng/api";
 
 @Component({
   selector: 'app-user-register',
@@ -6,6 +11,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-register.component.css']
 })
 export class UserRegisterComponent {
+  message : Message[] | undefined;
+  formGroup!: FormGroup;
+  formBuilder: FormBuilder = inject(FormBuilder);
+  isPasswordValid: boolean = false;
+  constructor() {
+  }
+  ngOnInit(){
+    this.formGroup = this.formBuilder.group({
+      firstName: new FormControl<string>(''),
+      lastName: new FormControl<String | null>(null),
+      username: new FormControl<string>(''),
+      password: new FormControl<string>(''),
+      confirmPassword: new FormControl<string>(''),
 
-    protected readonly Screen = Screen;
+    });
+
+  }
+
+  passwordPolicy(): boolean {
+
+    //regex to check if password contains a number
+    const passwordRegex = new RegExp("^(?=.*[0-9])");
+
+    const password = this.formGroup.get('password')?.value;
+    const confirmPassword = this.formGroup.get('confirmPassword')?.value;
+    return passwordRegex.test(password) && password;
+
+  }
+  onSubmit() {
+    this.isPasswordValid = this.passwordPolicy();
+    if (!this.isPasswordValid) {
+      this.displayMessage("La contraseña debe contener al menos un número", "error");
+    }
+    if(this.formGroup.get('password')?.value != this.formGroup.get('confirmPassword')?.value){
+      this.displayMessage("Las contraseñas no coinciden", "error");
+    }
+  }
+  displayMessage(message: string, severity: string){
+    this.message = [{ severity: severity, summary: 'Error!', detail: message }];
+  }
+
+
 }
+
